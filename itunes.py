a3f696484130886072aac0e5a0f0c83032b2d7c7
collinsjie <37666095+collinsjie@users.noreply.github.com>
2023-04-14 15:39:21 +0100

    Add files via upload

---
+++ itunes.py
@@ -0,0 +1,62 @@
+Music_app = {
+    "AFRO" : {"BURNA":{"OUTSIDE": ["YE", "SEKKLE DOWN", "HEAVENS GATE"],
+                       "AFRICAN GIANT":["SECRETS", 'KILLINDEM', 'DIFFERENT'],
+                        "LOVE DAMINI":['SOLID','GLORY','VANILLA']
+                        },
+
+               "DAVIDO":{"A BETTER TIME": ["THE BEST", "ON MY WAY", "SO CRAZY"],
+                       "TIMELESS":["AWAY", 'COMPETITION', 'NO MONEY'],
+                         "A GOOD TIME":['CHECK AM','DISTURBANCE','INTRO']
+                        }, 
+
+               "WIZKID":{"MIL": ["RECKLESS", "GINGER", "MIGHTYWINE"],
+                       "MLLE":["SPECIAL", 'FLOWERPADS', 'SUGAR'],
+                         "AYO":['JOY','AYO','PARTY']
+                        }
+            },
+
+    "GOSPEL": {"MERCY":{"SATISFIED": ["UDEME", "ONYE", "EXCCESS LOVE"],
+                       "THE CROSS":["REST", 'WITH ALL MY HEART', 'REGULAR'],
+                         "ELEVATED":['LIFTER','IMELA','CONFIDENCE']
+                        } , 
+
+                "MOSES":{"MOSES ALBUM 1": ["SONG1", "SONG3", "SONG3"],
+                       "MOSES ALBUM 2":["SONG4", 'SONG5', 'SONG6'],
+                         "MOSES ALBUM 3":['SONG7','SONG8','SONG9']
+                        }, 
+
+                "NATHANIEL BASSEY":{"NAMES OF GOD": ["ADONAI", "THE LORD IS MY LIGHT", "JESUS"],
+                       "HELLELUJAH LIFE":["SONG OF MANY WATERS", 'ESE', 'LET YOUR FIRE FALL'],
+                        "THE KING IS COMING":['HOLY','PSALM91','JEHOVAH NISSI']
+                        }
+            },
+
+    "FUJI": {"K1":{"FUJI THE SOUND": ["AWADE", "ADE ORI OKIN", "THINKING ABOUT YOU"],
+                    "VOLUME 2":["MUSIC ICON", 'IN THE 80', 'SOLO MAKINDE'],
+                    "OMO NINJA":['SUN SUN','AWATUNDE','OMO NINJA']
+                } , 
+
+            "PASUMA":{"MMM": ["MMM1", "MMM2", "MMM3"],
+                       "ABILITY PART 1":["ABILITY1", 'ABILITY2', 'ABILITY3'],
+                        "SO FAR SO GOOD":['GOOD1','GOOD2','GOOD3']
+                    }, 
+
+            "OSUPA":{"DIRECTION": ["LABALABA", "ORE NI WON", "LONDON DELIGHT"],
+                       "POWER OF MUSIC":["POWER1", 'POWER2', 'POWER3'],
+                         "FUJI TEMPLATE":['TEMPLATE1','TEMPLATE2','TEMPLATE3']
+                    }
+            },
+}
+
+print(Music_app)
+
+# adding items to the Music_app dictionary
+# Music_app.update({'AFRO':{'OSUPA':{'ALBUM1':['S1', 'S2', 'S3']}}}),
+# print(Music_app)
+# ['AFRO'].update({'Davide':{'ALBUM1':['S1,', 'S2', 'S3']}})
+# print(itunes)
+# Music_app['AFRO'].update({'BURNA':{'OUTSIDE':['NEW OUTSIDE SONG']}})
+# print(Music_app)
+
+
+
